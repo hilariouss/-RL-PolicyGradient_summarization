f3e4a4909231b2694ed301901f77539175f1f6dc
Dohyun Kwon <41457511+hilariouss@users.noreply.github.com>
2019-03-15 07:51:32 +0900

    Update A3C.py

--- 5. A3C/A3C.py
+++ 5. A3C/A3C.py
@@ -26,4 +26,37 @@ parser.add_argument('--MAX_EPISODE', type=int, default=1000,
                     help='Max number of episode')
 parser.add_argument('--GAME', default='CartPole-v0',
                     help='Game name')
-parser.add_argument('')
+parser.add_argument('--LOG_DIR', default='./log',
+                    help='log directory')
+parser.add_argument('--OUTPUT_GRAPH', default='True',
+                    help='Tensorflow graph restoration')
+parser.add_argument('--NUMBER_WORKERS', type=int, default=multiprocessing.cpu_count(),
+                    help='The number of workers in parallel learning')
+parser.add_argument('--GAMMA', type=float, default=0.99,
+                    help='Discount factor')
+parser.add_argument('--GLOBAL_NET_SCOPE', default='Global_Net',
+                    help='Global network scope')
+parser.add_argument('--LEARNING_RATE_ACTOR', type=float, default=0.001,
+                    help='learning rate of actor')
+parser.add_argument('--LEARNING_RATE_CRITIC', type=float, default=0.001,
+                    help='learning rate of critic')
+parser.add_argument('--ENTROPY_BETA', type=float, default=0.001,
+                    help='Entropy beta')
+parser.add_argument('--UPDATE_GLOBAL_ITER', type=int, default=10,
+                    help='Period of updating global net')
+hparams = parser.parse_args()
+
+env = gym.make(hparams.GAME)
+env.seed(1)
+tf.set_random_seed(1)
+np.random.seed(1)
+
+n_state_space = env.observation_space.shape[0]
+n_action_space = env.action_space.n
+
+GLOBAL_EPISODE = 0
+GLOBAL_REWARD = []
+
+
+
+
